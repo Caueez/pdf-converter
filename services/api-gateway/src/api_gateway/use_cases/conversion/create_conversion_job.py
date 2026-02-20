cadd30d14386d63c596b20b52db2d990b340c778
Caue Miguel <caue@caue-note.caue-note>
2026-02-19 21:09:40 -0300

    refact use_cases api-gateway

--- services/api-gateway/src/api_gateway/use_cases/conversion/create_conversion_job.py
+++ services/api-gateway/src/api_gateway/use_cases/conversion/create_conversion_job.py
@@ -6,6 +6,7 @@ from typing import Any
 import uuid
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 from infra.http.interface.http import HttpInterface
 
 
@@ -31,13 +32,18 @@ class CreateConversionJobUseCase:
             #     "presigned_url": "https://example.com/presigned_url"
             #     }
 =======
+=======
+from infra.http.interface.http import HttpInterface
+
+>>>>>>> 1a86441 (partial: implement http_client)
 
 
 class CreateConversionJobUseCase:
-    def __init__(self, http_client : HTTPClient) -> None:
+    def __init__(self, http_client : HttpInterface) -> None:
         self.http_client = http_client
 
     async def execute(self, conversion_config: str) -> dict[str, Any]:
+<<<<<<< HEAD
         return {
             "conversion_config": conversion_config,
             "job_id": str(uuid.uuid4()),
@@ -46,3 +52,20 @@ class CreateConversionJobUseCase:
             "presigned_url": "https://example.com/presigned_url"
             }
 >>>>>>> 169784b (partial: api-gateway service implementation)
+=======
+        try:
+            response = await self.http_client.post("http://conversion-service:8000/conversion", data=conversion_config)
+            return response
+        except Exception as e:
+            raise Exception(e)
+
+        
+        
+            # return {
+            #     "conversion_config": conversion_config,
+            #     "job_id": str(uuid.uuid4()),
+            #     "status": "pending",
+            #     "message": "Conversion job created",
+            #     "presigned_url": "https://example.com/presigned_url"
+            #     }
+>>>>>>> 1a86441 (partial: implement http_client)
--- services/api-gateway/src/api_gateway/use_cases/conversion/get_conversion_job.py
+++ services/api-gateway/src/api_gateway/use_cases/conversion/get_conversion_job.py
@@ -6,6 +6,7 @@ from typing import Any
 import uuid
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 from infra.http.interface.http import HttpInterface
 
 class GetConversionJobUseCase:
@@ -20,14 +21,25 @@ class GetConversionJobUseCase:
             raise Exception(e)
 =======
 
+=======
+from infra.http.interface.http import HttpInterface
+>>>>>>> 1a86441 (partial: implement http_client)
 
 class GetConversionJobUseCase:
-    def __init__(self, http_client : HTTPClient) -> None:
+    def __init__(self, http_client : HttpInterface) -> None:
         self.http_client = http_client
 
     async def execute(self, job_id: str) -> dict[str, Any]:
+<<<<<<< HEAD
         return {
             "job_id": job_id,
             "status": "pending"
             }
 >>>>>>> 169784b (partial: api-gateway service implementation)
+=======
+        try:
+            response = await self.http_client.get(f"http://conversion-service:8000/conversion/{job_id}")
+            return response
+        except Exception as e:
+            raise Exception(e)
+>>>>>>> 1a86441 (partial: implement http_client)
